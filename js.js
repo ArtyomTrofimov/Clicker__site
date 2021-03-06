7d0e7896e443310b4d1aa873e74a21b4c4917b27
Mikhail <pony4swag@gmail.com>
2021-03-06 21:32:16 +0300

    add favicon

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var click_score = 100000000;
+var click_score = 1;
 var upgrade_mult = 1;
 var gnom_lvl = 0;
 
@@ -36,7 +36,6 @@ function auto() {
     if (gnom_lvl >= 1) {
         document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('gnom1').value);
     }
-
 
 }
 
