7208cc8cc4f84091809f01533bbf0f7f252ce552
Artyom <trofimovartem999@gmail.com>
2021-03-07 21:12:06 +0300

    Сделал фермы и кузни

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
-var click_score = 1;
-var upgrade_mult = 1;
-var gnom_lvl = 0;
+var click_score = 100000;
+var click_lvl = 1;
+var upgrade_mult = 2;
+
 
 function Clean() {
     document.getElementById('score').value = 0;
@@ -11,31 +12,76 @@ function Click() {
 }
 
 function upgrade_click() {
-    if (eval(document.getElementById('score').value) >= 10 * upgrade_mult) {
-        (document.getElementById('score').value) -= (document.getElementById('upgrade1').value) * upgrade_mult;
-        click_score += 1;
+    if (eval(document.getElementById('score').value) >= 5 * Math.pow(10, upgrade_mult) && click_lvl <= 2) {
+        (document.getElementById('score').value) -= 5 * Math.pow(10, upgrade_mult);
+
+        if (click_lvl == 1) {
+            click_score += 4;
+            click_lvl += 1;
+            document.getElementById("upgrade_click").innerText = 'Стоимость: ' + 5 * Math.pow(10, upgrade_mult + 1) + ' монет';
+        } else if (click_lvl == 2) {
+            click_score += 45;
+            click_lvl += 1;
+            document.getElementById('upgrade_click').innerText = 'Вы максимально улучшили клик';
+        }
         upgrade_mult += 1;
-        document.getElementById("upgrade1").innerText = document.getElementById("upgrade1").value * upgrade_mult + ' рублей'
+
+    } else if (eval(document.getElementById('score').value) < 5 * Math.pow(10, upgrade_mult) && click_lvl != 3) {
+        alert('Недостаточно монет');
     } else {
-        alert('Недостаточно средств');
+        document.getElementById('score').innerText = 'Вы максимально улучшили клик';
+    }
+
+}
+
+var farm_lvl = 0;
+
+function upgrade_farm() {
+    if (eval(document.getElementById('score').value) >= 100 + 20 * farm_lvl && farm_lvl <= 9) {
+        (document.getElementById('score').value) -= 100 + 20 * farm_lvl;
+        if (farm_lvl < 9) {
+            document.getElementById('farm').value = eval(document.getElementById('farm').value) + 2;
+            farm_lvl += 1;
+            document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nСтоимость: ' + (100 + 20 * farm_lvl) + ' монет';
+        } else if (farm_lvl == 9) {
+            document.getElementById('farm').value = eval(document.getElementById('farm').value) + 2;
+            farm_lvl += 1;
+            document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nВы максимально улучшили ферму';
+        }
+    } else if (eval(document.getElementById('score').value) < 100 + 20 * farm_lvl && farm_lvl <= 9) {
+        alert('Недостаточно монет');
+    } else {
+        document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nВы максимально улучшили ферму';
     }
 }
-function upgrade_gnom() {
-    if (eval(document.getElementById('score').value) >= 100 * (gnom_lvl + 1)) {
-        (document.getElementById('score').value) -= 100 * (gnom_lvl + 1);
-        document.getElementById('gnom1').value = eval(document.getElementById('gnom1').value) + 0.5;
-        gnom_lvl += 1;
-        document.getElementById("gnom1").innerText = 'Gnom X ' + gnom_lvl + '\n' + (100 * (gnom_lvl + 1)) + ' рублей';
+
+var forge_lvl = 0;
+
+function upgrade_forge() {
+    if (eval(document.getElementById('score').value) >= 1000 + 250 * forge_lvl && forge_lvl <= 9) {
+        (document.getElementById('score').value) -= 1000 + 250 * forge_lvl;
+        if (forge_lvl < 9) {
+            document.getElementById('forge').value = eval(document.getElementById('forge').value) + 6;
+            forge_lvl += 1;
+            document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nСтоимость: ' + (1000 + 250 * forge_lvl) + ' монет';
+        } else if (forge_lvl == 9) {
+            document.getElementById('forge').value = eval(document.getElementById('forge').value) + 6;
+            forge_lvl += 1;
+            document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nВы максимально улучшили кузницу';
+        }
+    } else if (eval(document.getElementById('score').value) < 1000 + 250 * forge_lvl && forge_lvl <= 9) {
+        alert('Недостаточно монет');
     } else {
-        alert('Недостаточно средств');
+        document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nВы максимально улучшили кузницу';
     }
 }
+
+
 
 function auto() {
 
-    if (gnom_lvl >= 1) {
-        document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('gnom1').value);
-    }
+    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('farm').value);
+    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('forge').value);
 
 }
 
