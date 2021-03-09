a63e6cf023b93878e82e55873b2364a45f84ee3c
Artyom <trofimovartem999@gmail.com>
2021-03-09 12:36:54 +0300

    Перенёс функцию для автоматизации в отдельный файл, написал общий код для улучшения зданий

--- js.js
+++ js.js
@@ -34,56 +34,29 @@ function upgrade_click() {
 
 }
 
-var farm_lvl = 0;
+function upgrade(elem) {
 
-function upgrade_farm() {
-    if (eval(document.getElementById('score').value) >= 100 + 20 * farm_lvl && farm_lvl <= 9) {
-        (document.getElementById('score').value) -= 100 + 20 * farm_lvl;
-        if (farm_lvl < 9) {
-            document.getElementById('farm').value = eval(document.getElementById('farm').value) + 2;
-            farm_lvl += 1;
-            document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nСтоимость: ' + (100 + 20 * farm_lvl) + ' монет';
-        } else if (farm_lvl == 9) {
-            document.getElementById('farm').value = eval(document.getElementById('farm').value) + 2;
-            farm_lvl += 1;
-            document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nВы максимально улучшили ферму';
-        }
-    } else if (eval(document.getElementById('score').value) < 100 + 20 * farm_lvl && farm_lvl <= 9) {
+    var incom = parseInt(elem.value);
+    var incomInc = parseInt(elem.dataset.valueInc)
+    var price = parseInt(elem.dataset.price);
+    var lvl = parseInt(elem.dataset.lvl);
+    var priceScale = parseInt(elem.dataset.priceInc);
+
+    if (eval((document.getElementById('score').value)) >= price + priceScale * lvl) {
+        (document.getElementById('score').value) -= price + priceScale * lvl;
+        elem.value = incom + incomInc;
+        elem.dataset.lvl = lvl + 1;
+        elem.innerText = elem.dataset.name + '\n' + elem.dataset.lvl + '\nСтоимость: ' + (price + priceScale * (lvl + 1)) + ' монет';
+        document.getElementById('incom').value = parseInt(document.getElementById('incom').value) + parseInt(elem.dataset.valueInc) + '/sec';
+
+    } else if (eval(document.getElementById('score').value) < price + priceScale * lvl) {
         alert('Недостаточно монет');
-    } else {
-        document.getElementById("farm").innerText = 'Ферма\nУровень: ' + farm_lvl + '\nВы максимально улучшили ферму';
     }
-}
 
-var forge_lvl = 0;
 
-function upgrade_forge() {
-    if (eval(document.getElementById('score').value) >= 1000 + 250 * forge_lvl && forge_lvl <= 9) {
-        (document.getElementById('score').value) -= 1000 + 250 * forge_lvl;
-        if (forge_lvl < 9) {
-            document.getElementById('forge').value = eval(document.getElementById('forge').value) + 6;
-            forge_lvl += 1;
-            document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nСтоимость: ' + (1000 + 250 * forge_lvl) + ' монет';
-        } else if (forge_lvl == 9) {
-            document.getElementById('forge').value = eval(document.getElementById('forge').value) + 6;
-            forge_lvl += 1;
-            document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nВы максимально улучшили кузницу';
-        }
-    } else if (eval(document.getElementById('score').value) < 1000 + 250 * forge_lvl && forge_lvl <= 9) {
-        alert('Недостаточно монет');
-    } else {
-        document.getElementById("forge").innerText = 'Кузница\nУровень: ' + forge_lvl + '\nВы максимально улучшили кузницу';
-    }
+
 }
 
 
 
-function auto() {
-
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('farm').value);
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('forge').value);
-
-}
-
-setInterval(auto, 1000);
 
