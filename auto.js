cbedf558d63ad7c7a3b5d116a89fd963d48351e7
Artyom <trofimovartem999@gmail.com>
2021-03-10 15:31:43 +0300

    You all know

--- auto.js
+++ auto.js
@@ -1,11 +1,11 @@
-function auto() {
 
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('farm').value);
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('forge').value);
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('inn').value);
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('barracks').value);
-    document.getElementById('score').value = eval(document.getElementById('score').value) + eval(document.getElementById('mine').value);
+function auto__inc_check() {
+    document.getElementById('incom').value = 0;
+    var int = parseInt(document.getElementById('incom').value);
+    var str = document.getElementsByName('build');
+    for (i = 0; i < str.length; i++) {
+        int += parseInt(str[i].value);
 
+    }
+    document.getElementById('incom').value = int + '/sec';
 }
-
-setInterval(auto, 1000);
