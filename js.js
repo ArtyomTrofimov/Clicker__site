ec3a638a738f5d0ddbaba6865b868144049f9bdc
Artyom <trofimovartem999@gmail.com>
2021-03-11 21:10:06 +0300

    Переработал функции
    Переименовал auto2 в auto_inc
    В js и index добавил здание Castle 
    Поменял значения и сбалансировал

--- js.js
+++ js.js
@@ -1,16 +1,17 @@
-var click_score = 100000;
+var click_score = 100000000;
 var click_lvl = 1;
 var upgrade_mult = 2;
 
-
-function Clean() {
-    document.getElementById('score').value = 0;
-}
-
 function Click() {
-    document.getElementById('score').value = eval(document.getElementById('score').value) + click_score;
+    document.getElementById('score').value = parseInt(+document.getElementById('score').value) + click_score;
     document.getElementById('audio1').play()
+
 }
+
+function strReplace(str) {
+    return str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+}
+
 
 function upgrade_click() {
     if (eval(document.getElementById('score').value) >= 5 * Math.pow(10, upgrade_mult) && click_lvl <= 2) {
@@ -65,14 +66,14 @@ function boost_inc(elem) {
     var str = inc.join('');
     var upgrade = document.getElementById(str);
     var lvl = parseInt(elem.dataset.lvl);
-    if (eval((document.getElementById('score').value)) >= parseInt(elem.dataset.price) && lvl < 2) {
-        if (lvl < 1) {
+    if (eval((document.getElementById('score').value)) >= parseInt(elem.dataset.price) && lvl < 9) {
+        if (lvl < 8) {
             (document.getElementById('score').value) -= parseInt(elem.dataset.price);
             elem.dataset.lvl = lvl + 1;
             elem.dataset.price = parseInt(elem.dataset.price) + parseInt(elem.dataset.price) * (parseInt(elem.dataset.priceInc) - 1) * parseInt(elem.dataset.lvl);
             upgrade.dataset.incomBoost = parseInt(upgrade.dataset.incomBoost) + 1;
-            elem.innerText = 'Общий множитель "' + upgrade.dataset.name + '": ' + (parseInt(upgrade.dataset.incomBoost) * 100) + '%\n' + upgrade.dataset.name + ' + 100%\nУровень: ' + elem.dataset.lvl + '\nСтоимость: ' + parseInt(elem.dataset.price) + 'монет';
-        } else if (lvl == 1) {
+            elem.innerText = 'Общий множитель "' + upgrade.dataset.name + '": ' + (parseInt(upgrade.dataset.incomBoost) * 100) + '%\n' + upgrade.dataset.name + ' + 100%\nУровень: ' + elem.dataset.lvl + '\nСтоимость: ' + parseInt(elem.dataset.price) + ' монет';
+        } else if (lvl == 8) {
             (document.getElementById('score').value) -= parseInt(elem.dataset.price);
             elem.dataset.lvl = lvl + 1;
             elem.dataset.price = parseInt(elem.dataset.price) + parseInt(elem.dataset.price) * (parseInt(elem.dataset.priceInc) - 1) * parseInt(elem.dataset.lvl);
@@ -81,14 +82,10 @@ function boost_inc(elem) {
         }
 
         auto__inc_check();
-    } else if (eval((document.getElementById('score').value)) < parseInt(elem.dataset.price) && parseInt(elem.dataset.lvl) < 2) {
+    } else if (eval((document.getElementById('score').value)) < parseInt(elem.dataset.price) && parseInt(elem.dataset.lvl) < 8) {
         alert('Недостаточно монет')
     } else {
         elem.innerText = 'Общий множитель "' + upgrade.dataset.name + '": ' + (parseInt(upgrade.dataset.incomBoost) * 100) + '%\n' + upgrade.dataset.name + ' \nУровень: ' + elem.dataset.lvl + '\nВы купили все улучшения для этого здания ';
     }
 }
-
-
-
-
 
